1f187e368e70f6a5d6b90079d37509a3c046e1c7
xelfus23 <xelfus23@gmail.com>
2024-03-13 22:00:54 +0800

    modified ko

---
+++ JavaScript/javascript.js
@@ -0,0 +1,8 @@
+
+function clicked(){
+  let firstname = document.getElementById("input-1");
+  let lastname = document.getElementById("input-2");
+  document.getElementById("play").innerHTML = firstname + lastname;
+}
+clicked();
+
